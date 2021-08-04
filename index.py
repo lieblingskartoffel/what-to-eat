9bebe8ea49dfd9b264bd25031a64bb1054bc1285
Caroline Wells <carolinewells93@gmail.com>
2021-08-04 15:52:50 -0400

    add functionality for storing and displaying which dishes were liked

--- index.py
+++ index.py
@@ -14,9 +14,25 @@ def choose_random_restaurant(restaurants):
     return restaurants[chosen_index]
 
 
+def get_liked_dishes():
+    liked_dishes_file = open('liked_dishes.json')
+    liked_dishes = json.load(liked_dishes_file)
+    return liked_dishes
+
+
+def get_liked_dishes_for_restaurant(liked_dishes, restaurant_name):
+    liked_dishes_for_given_restaurant = [liked_dish for liked_dish in liked_dishes.get('liked_dishes') if liked_dish.get('restaurant_name') == restaurant_name]
+    liked_dishes_string = ''
+    for dish in liked_dishes_for_given_restaurant:
+        liked_dishes_string += dish.get('who_liked_it') + ' liked ' + dish.get('dish_name') + '.\n'
+    return liked_dishes_string
+
+
 # Main execution
 gotten_restaurants = get_restaurants()
 gotten_restaurants = gotten_restaurants.get('restaurants')
 chosen_restaurant = choose_random_restaurant(gotten_restaurants)
-print('Order from ' + chosen_restaurant.get('name') + ' (' + chosen_restaurant.get('cuisine') + ') on '
-      + chosen_restaurant.get('app'))
+chosen_restaurant_name = chosen_restaurant.get('name')
+liked_dishes_for_restaurant = get_liked_dishes_for_restaurant(get_liked_dishes(), chosen_restaurant_name)
+print('Order from ' + chosen_restaurant_name + ' (' + chosen_restaurant.get('cuisine') + ') on '
+      + chosen_restaurant.get('app') + '.\n' + liked_dishes_for_restaurant)
